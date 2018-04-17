97047857597c5b50e40de94b55b625edc83e64c5
James Moran <jojo.2nd.jm@gmail.com>
2018-04-17 22:20:06 +0100

    Added simple colour test

--- src/main.c
+++ src/main.c
@@ -11,6 +11,30 @@
 
 #define MAKE_U32_COLOR(r, g, b, a) (uint32_t)(((r&0xFF)  << 24) | ((g&0xFF) << 16) | ((b&0xFF) << 8) | ((a&0xFF) << 0))
 
+struct ray_t {
+  vec3_t pt, dir;
+};
+typedef struct ray_t ray_t;
+
+vec3_t ray_point_at(ray_t* ray, float t) {
+  vec3_t r, vt;
+  vmathV3ScalarMul(&vt, &ray->dir, t);
+  vmathV3Add(&r, &ray->pt, &vt);
+  return r;
+}
+
+vec3_t colour(const ray_t* ray) {
+  static const vec3_t k1 = {1.f, 1.f, 1.f};
+  static const vec3_t k2 = {.5f, .7f, 1.f};
+  vec3_t unit_dir, a, b, ret;
+  vmathV3Normalize(&unit_dir, &ray->dir);
+  float t = .5f * (unit_dir.y + 1.f);
+  vmathV3ScalarMul(&a, &k1, 1.0f-t);
+  vmathV3ScalarMul(&b, &k2, t);
+  vmathV3Add(&ret, &a, &b);
+  return ret;  
+}
+
 int main(int argc, char** argv) {
   if (SDL_Init(SDL_INIT_VIDEO)!=0) {
     fprintf(stderr, "Error calling SDL_Init");        
@@ -38,14 +62,25 @@ int main(int argc, char** argv) {
     int dest_main_bfr_pitch;
     SDL_LockTexture(main_surface, NULL, (void**)&dest_main_buffer, &dest_main_bfr_pitch);
 
+    vec3_t lower_left_corner = {-2.f, -1.f, -1.f};
+    vec3_t horizontal = {4.f, 0.f, 0.f};
+    vec3_t vertical = {0.f, 2.f, 0.f};
+    vec3_t origin = {0.f, 0.f, 0.f};
     for (int32_t j=SCRN_HEIGHT-1; j >= 0; --j) {
       for (int32_t i=0; i < SCRN_WIDTH; ++i) {
-        float r = (float)i / (float)SCRN_WIDTH;
-        float g = (float)j / (float)SCRN_HEIGHT;
-        float b = .2f;
-        int ir = round(255.99*r);
-        int ig = round(255.99*g);
-        int ib = round(255.99*b);
+        float u = (float)i / (float)SCRN_WIDTH;
+        float v = (float)j / (float)SCRN_HEIGHT;
+        vec3_t vv;
+        ray_t screen_ray;
+        vmathV3ScalarMul(&screen_ray.dir, &horizontal, u);
+        vmathV3ScalarMul(&vv, &vertical, v);
+        vmathV3Add(&screen_ray.dir, &screen_ray.dir, &vv);
+        vmathV3Add(&screen_ray.dir, &screen_ray.dir, &lower_left_corner);
+        screen_ray.pt = origin;
+        vec3_t col = colour(&screen_ray);
+        int ir = round(255.99*col.x);
+        int ig = round(255.99*col.y);
+        int ib = round(255.99*col.z);
 
         *(uint32_t*)(((uint8_t*)dest_main_buffer) + dest_main_bfr_pitch*j + i*4) = 
         MAKE_U32_COLOR(ir, ig, ib, 255);
